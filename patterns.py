a9a5f33b68d11b1b92ca3b6e1352d79a17d7eb83
Kavy Jain <jkavy5114@gmail.com>
2026-09-15 13:59:45 +0530

    20 patterns practiced to improvise understanding of problems.

--- patterns.py
+++ patterns.py
@@ -7,7 +7,7 @@ while True:
         continue
 
     break
-print()
+
 
 #Pattern 1: Solid Square
 
@@ -118,7 +118,7 @@ print()
 
 #     print()
 
-#Pattern 11: Repeated Number Pyramid
+#Pattern 12: Repeated Number Pyramid
 
 # for row in range(1, rows+1):
 #     for spaces in range(rows-row):
@@ -129,7 +129,7 @@ print()
 
 #     print()
 
-#Pattern 12: Inverted Number Pyramid
+#Pattern 13: Inverted Number Pyramid
 
 # for row in range(rows, 0, -1):
 #     for spaces in range(rows-row):
@@ -138,9 +138,9 @@ print()
 #     for col in range(1, 2*row):
 #         print(col, end = " ")
 
-#    print()
+#     print()
 
-#Pattern 13:
+#Pattern 14:
 # count=1
 # for row in range(1, rows+1):
     
@@ -150,7 +150,87 @@ print()
 
 #     print()
 
-#Patter 14: 
+#Pattern 15: 
+
+# for row in range(1, rows+1):
+
+#     for col in range(row):
+#         if (row+col)%2 == 0:
+#             print(1, end= " ")
+#         else:
+#             print(0, end = " ")
+#     print()
+
+#Pattern 16:
+
+# for row in range(1, rows+1):
+    
+#     for col in range(1, rows+1):
+#         if col == 1 or col == rows or row == 1 or row == rows:
+#             print("*", end = " ")
+#         else:
+#             print(" ", end = " ")
+#     print()
+    
+#Pattern 17: Hollow Triangle
+
+# for row in range(1, rows+1):
+#     for col in range(1, row+1):
+#         if row == 1 or row == rows or col==1 or col == row:
+#             print("*", end = " ")
+#         else:
+#             print(" ", end = " ")
+#     print()
 
 
-print()    
+#Pattern 18: X
+
+# for row in range(1, rows+1):
+#     for col in range(1, rows+1):
+#         if col == row or col == row-row+1:
+#             print("*", end = " ")
+#         else:
+#             print(" ", end = " ")
+#     print()
+
+#Pattern  19:
+
+# for row in range (rows+1):
+
+#     for col in range(row):
+#         print("*", end = " ")
+
+#     for space in range(2*(rows-row)):
+#         print(" ", end = " ")
+
+#     for col in range(row):
+#         print("*", end = " ")
+       
+#     print()
+
+# for row in range (rows-1, 0, -1):
+
+#     for col in range(row):
+#         print("*", end = " ")
+
+#     for space in range(2*(rows-row)):
+#         print(" ", end = " ")
+
+#     for col in range(row):
+#         print("*", end = " ")
+       
+#     print()
+
+for row in range(1, 2*rows):
+
+    if row<=rows:
+        current_row = row 
+    else: 
+        current_row = 2 * rows - row
+
+    for col in range(2*rows):
+        if col < current_row or col >= 2 * rows - current_row:
+            print("*", end = " ")
+        else:
+            print(" ", end  = " ")
+    print()
